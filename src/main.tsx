f69eb889a7b53721cb5a8cc0d8dffc6c40c987ad
Kelly  Arseneau <168972251+kelly12201984@users.noreply.github.com>
2025-11-24 11:00:15 -0500

    Update routing to replace LandingPage and FarmDetailPage with Dashboard component

--- src/main.tsx
+++ src/main.tsx
@@ -2,8 +2,7 @@ import React from 'react'
 import ReactDOM from 'react-dom/client'
 import { QueryClient, QueryClientProvider } from '@tanstack/react-query'
 import { BrowserRouter, Routes, Route, Navigate } from 'react-router-dom'
-import LandingPage from './pages/LandingPage'
-import FarmDetailPage from './pages/FarmDetailPage'
+import Dashboard from './pages/Dashboard'
 import './index.css'
 
 const queryClient = new QueryClient({
@@ -21,8 +20,7 @@ ReactDOM.createRoot(document.getElementById('root')!).render(
       <BrowserRouter>
         <Routes>
           <Route path="/" element={<Navigate to="/readings" replace />} />
-          <Route path="/readings" element={<LandingPage />} />
-          <Route path="/readings/farm/:farmId" element={<FarmDetailPage />} />
+          <Route path="/readings" element={<Dashboard />} />
         </Routes>
       </BrowserRouter>
     </QueryClientProvider>
